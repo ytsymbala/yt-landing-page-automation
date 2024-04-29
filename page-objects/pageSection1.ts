12455ad8880a246451e0abb9190f849f86333497
Yurii Tsymbala <tsymbala.yuriy@gmail.com>
2024-04-29 21:58:04 +0300

    moved test data from page object to test data file

--- page-objects/pageSection1.ts
+++ page-objects/pageSection1.ts
@@ -1,5 +1,5 @@
 import { Page, Locator, expect } from '@playwright/test';
-import { expectedSitreLogoSrc, expectedUserLogoSrc, headerlinksToTest, hoveredSrc, userItemsLinksToTest } from '../test-data';
+import { expectedContentLink, expectedFirstDescription, expectedHeadingText, expectedRubricText, expectedSecondDescription } from '../test-data';
 
 export class PageSection1 {
     readonly page: Page;
@@ -28,31 +28,26 @@ export class PageSection1 {
 
     async showSection1RubricText() {
         const getsection1RubricText = await this.section1Rubric.textContent();
-        const expectedRubricText = 'NEW DESIGN';
         expect(getsection1RubricText).toContain(expectedRubricText);
     }
 
     async showSection1HeaderText() {
         const getSection1Heading = await this.section1Heading.textContent();
-        const expectedHeadingText = 'There is no other platforms for you as like';
         expect(getSection1Heading).toContain(expectedHeadingText);
     }
 
     async showSection1ContentLink() {
         const getSection1ContentLink = await this.section1ContentLink.textContent();
-        const expectedContentLink = '....';
         expect(getSection1ContentLink).toContain(expectedContentLink);
     }
 
     async showSection1FirstDescription() {
         const getSection1FirstDescription = await this.section1FirstDescription.textContent();
-        const expectedFirstDescription = 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.'
         expect(getSection1FirstDescription).toContain(expectedFirstDescription);
     }
 
     async showSection1SecondDescription() {
         const getSection1SecondDescription = await this.section1SecondDescription.textContent();
-        const expectedSecondDescription = 'Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur. Nulla vitae elit libero, a pharetra augue.'
         expect(getSection1SecondDescription).toContain(expectedSecondDescription);
     }
 }
