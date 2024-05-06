f79dc2a4e765e9c148d040738fb6b470313765f1
Yurii Tsymbala <tsymbala.yuriy@gmail.com>
2024-05-06 22:26:14 +0300

    Updated Page Object, moved test data to the separate file

--- page-objects/pageSection2.ts
+++ page-objects/pageSection2.ts
@@ -1,5 +1,5 @@
 import { Page, Locator, expect } from '@playwright/test';
-import { expectedBannerNewsHeadigText, expectedBannerNewsRubricText, expectedBlocks } from '../test-data';
+import { expectedSection2ContentLink, expectedSection2ContentText, expectedSection2HeadingText, expectedSection2ImageSrc, expectedSection2RubricText } from '../test-data';
 
 export class PageSection2 {
     readonly page: Page;
@@ -27,35 +27,30 @@ export class PageSection2 {
     async showSection2RubricText() {
         await expect(this.section2RubricText).toBeVisible();
         const getSection2RubricText = await this.section2RubricText.textContent();
-        const expectedSection2RubricText = 'NEW DESIGN';
         expect(getSection2RubricText).toContain(expectedSection2RubricText);
     }
 
     async showSection2HeadingText() {
         await expect(this.section2HeadingText).toBeVisible();
         const getSection2HeadingText = await this.section2HeadingText.textContent();
-        const expectedSection2HeadingText = 'Resposive design, just need your tap';
         expect(getSection2HeadingText).toContain(expectedSection2HeadingText);
     }
 
     async showSection2ContentLink() {
         await expect(this.section2ContentLink).toBeVisible();
         const getSection2ContentLink = await this.section2ContentLink.textContent();
-        const expectedSection2ContentLink = '....';
         expect(getSection2ContentLink).toContain(expectedSection2ContentLink);
     }
 
     async showSection2ContentText() {
         await expect(this.section2ContentText).toBeVisible();
         const getSection2ContentText = await this.section2ContentText.textContent();
-        const expectedSection2ContentText = 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiasem malesuada magn. Vestibulum felis euismod semper.';
         expect(getSection2ContentText).toContain(expectedSection2ContentText);
     }
 
     async showSection2Image() {
         await expect(this.section2ImageSrc).toBeVisible();
         const getSection2ImageSrc = await this.section2ImageSrc.getAttribute('src');
-        const expectedSection2ImageSrc = 'images/macbook.png';
         expect(getSection2ImageSrc).toContain(expectedSection2ImageSrc);
     }
 }
