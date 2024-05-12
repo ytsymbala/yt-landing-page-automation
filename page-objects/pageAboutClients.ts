5952766f6ff4612756b9582f015237703ec259cf
Yurii Tsymbala <tsymbala.yuriy@gmail.com>
2024-05-12 23:23:26 +0300

    Moved test data for about clients to the separate file

--- page-objects/pageAboutClients.ts
+++ page-objects/pageAboutClients.ts
@@ -1,5 +1,5 @@
 import { Page, Locator, expect } from '@playwright/test';
-import { expectedSection3ContentText, expectedSection3HeadingText, expectedSection3ImageSrc, expectedSection3RubricText } from '../test-data';
+import { expectedClientsBlocks, expectedLogos, expectedSection3ContentText, expectedSection3HeadingText, expectedSection3ImageSrc, expectedSection3RubricText } from '../test-data';
 
 export class PageAboutClients {
     readonly page: Page;
@@ -20,31 +20,10 @@ export class PageAboutClients {
 
     async showClientsContent() {
         const getAboutClientsBlocks = await this.aboutClientsBlocks.all()
-
-        const expectedBlocks = [
-            {
-                image: 'images/user_1.svg',
-                name: 'Jonathon Doe',
-                title: 'Co Founder',
-                description: '“Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id gravida at eget metus. Etiasem malesuada magn”'
-            },
-            {
-                image: 'images/user_2.svg',
-                name: 'Jonathon Doe',
-                title: 'Co Founder',
-                description: '“Pellentesque ornare sem lacinia quam. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiasem malesuada magn”'
-            },
-            {
-                image: 'images/user_3.svg',
-                name: 'Jonathon Doe',
-                title: 'Co Founder',
-                description: '“Aenean eu leo quam. Pellentesque ornare sem lacinia qua emere wancerid elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiasem malesuada magn”'
-            }
-        ];
         
         for (let i = 0; i < getAboutClientsBlocks.length; i++) {
             const block = getAboutClientsBlocks[i];
-            const expectedBlock = expectedBlocks[i];
+            const expectedClientsBlock = expectedClientsBlocks[i];
             
             const imageElement = block.locator('img');
             const nameElement = block.locator('.client__name');
@@ -56,10 +35,10 @@ export class PageAboutClients {
             const titleText = await titleElement.textContent();
             const textContent = await textElement.textContent();
             
-            expect(imageSrc).toContain(expectedBlock.image);
-            expect(nameText).toContain(expectedBlock.name);
-            expect(titleText).toContain(expectedBlock.title);
-            expect(textContent).toContain(expectedBlock.description);
+            expect(imageSrc).toContain(expectedClientsBlock.image);
+            expect(nameText).toContain(expectedClientsBlock.name);
+            expect(titleText).toContain(expectedClientsBlock.title);
+            expect(textContent).toContain(expectedClientsBlock.description);
         }
     }
 
@@ -67,14 +46,6 @@ export class PageAboutClients {
         await expect(this.clientsIconSection).toBeVisible();
 
         const logos = await this.clientsIconSection.locator('img').all();
-
-        const expectedLogos = [
-            'images/partner-logo-1.svg',
-            'images/partner-logo-2.svg',
-            'images/partner-logo-3.svg',
-            'images/partner-logo-4.svg',
-            'images/partner-logo-5.svg',
-        ];
     
         for (let i = 0; i < logos.length; i++) {
             const logo = logos[i];
