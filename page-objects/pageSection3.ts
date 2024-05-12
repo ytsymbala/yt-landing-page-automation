d935e8b5ac7eaebe1dd23fa16c5fac65c48aef2f
Yurii Tsymbala <tsymbala.yuriy@gmail.com>
2024-05-12 21:35:20 +0300

    Moved test data for section3 from the page object to the separate file

--- page-objects/pageSection3.ts
+++ page-objects/pageSection3.ts
@@ -1,5 +1,5 @@
 import { Page, Locator, expect } from '@playwright/test';
-import { expectedSection2ContentLink, expectedSection2ContentText, expectedSection2HeadingText, expectedSection2ImageSrc, expectedSection2RubricText } from '../test-data';
+import { expectedSection2ContentLink, expectedSection2ContentText, expectedSection2HeadingText, expectedSection2ImageSrc, expectedSection2RubricText, expectedSection3ContentText, expectedSection3HeadingText, expectedSection3ImageSrc, expectedSection3RubricText } from '../test-data';
 
 export class PageSection3 {
     readonly page: Page;
@@ -25,28 +25,24 @@ export class PageSection3 {
     async showSection3RubricText() {
         await expect(this.section3RubricText).toBeVisible();
         const getSection3RubricText = await this.section3RubricText.textContent();
-        const expectedSection3RubricText = 'NEW FEATURES';
         expect(getSection3RubricText).toContain(expectedSection3RubricText);
     }
 
     async showSection3HeadingText() {
         await expect(this.section3HeadingText).toBeVisible();
         const getSection3HeadingText = await this.section3HeadingText.textContent();
-        const expectedSection3HeadingText = 'Some awesone features';
         expect(getSection3HeadingText).toContain(expectedSection3HeadingText);
     }
 
     async showSection3ContentText() {
         await expect(this.section3ContentText).toBeVisible();
         const getSection3ContentText = await this.section3ContentText.textContent();
-        const expectedSection3ContentText = 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Etiasem malesuada magn. Vestibulum felis euismod semper.';
         expect(getSection3ContentText).toContain(expectedSection3ContentText);
     }
 
     async showSection3Image() {
         await expect(this.section3ImageSrc).toBeVisible();
         const getSection3ImageSrc = await this.section3ImageSrc.getAttribute('src');
-        const expectedSection3ImageSrc = 'images/safari.svg';
         expect(getSection3ImageSrc).toContain(expectedSection3ImageSrc);
     }
 }
